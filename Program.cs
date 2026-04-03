45c9a14c84d341cb67dccc2343bc497cb5c48a18
Lithemba Zazini <zazinilithemba@gmail.com>
2026-04-03 14:44:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace making_decisions___homework_random_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1; 
+            int max = 10;
+
+            Write("Guess a number from 1 -10: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(min, max);
+
+            if (num == randomNumber)
+                Write("Random number was {0}, you are correct" ,randomNumber);
+            else
+                if (num > randomNumber)
+                Write("Random number was {0}, too high" ,randomNumber);
+            else
+            {
+                Write("Random number was {0}, too low", randomNumber);
+            }
+            ReadLine();
+
+
+        }
+    }
+}
